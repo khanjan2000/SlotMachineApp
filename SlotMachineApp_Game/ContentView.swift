6d8903545a14f008277d76cffd5acb198e554824
Khanjan Vaidya <47827803+khanjan2000@users.noreply.github.com>
2022-10-02 20:33:11 +0200

    Add files via upload

---
+++ SlotMachineApp_Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotMachineApp_Game
+//
+//  Created by Vraj Patel on 17/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlotMachineApp_Game/SlotMachineApp_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp_GameApp.swift
+//  SlotMachineApp_Game
+//
+//  Created by Vraj Patel on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
